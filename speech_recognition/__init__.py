872a1bc439ba5e071eca164d4c0ebadae528eb59
Manxue Wang <mw3060@nyu.edu>
2016-11-22 12:16:28 -0800

    add listen_control function in Recognizer class to control speech record (press q to stop recording)

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -18,6 +18,8 @@ import json
 from googleapiclient import discovery
 import httplib2
 from oauth2client.client import GoogleCredentials
+import readchar
+import threading
 # [START authenticating]
 DISCOVERY_URL = ('https://{api}.googleapis.com/$discovery/rest?'
                  'version={apiVersion}')
@@ -483,6 +485,112 @@ class Recognizer(AudioSource):
             target_energy = energy * self.dynamic_energy_ratio
             self.energy_threshold = self.energy_threshold * damping + target_energy * (1 - damping)
 
+
+    def listen_control(self, source, timeout = None, phrase_time_limit = None):
+        """
+        Records a single phrase from ``source`` (an ``AudioSource`` instance) into an ``AudioData`` instance, which it returns.
+
+        This is done by waiting until the audio has an energy above ``recognizer_instance.energy_threshold`` (the user has started speaking), and then recording until it encounters ``recognizer_instance.pause_threshold`` seconds of non-speaking or there is no more audio input. The ending silence is not included.
+
+        The ``timeout`` parameter is the maximum number of seconds that it will wait for a phrase to start before giving up and throwing an ``speech_recognition.WaitTimeoutError`` exception. If ``timeout`` is ``None``, it will wait indefinitely.
+        """
+        isEndRecording = False
+
+        def endRecordingEventHandler():
+            global isEndRecording
+            while True:
+                ch = readchar.readchar()
+                if ch == 'q':
+                    isEndRecording = True
+                    break
+
+        assert isinstance(source, AudioSource), "Source must be an audio source"
+        assert source.stream is not None, "Audio source must be entered before listening, see documentation for `AudioSource`; are you using `source` outside of a `with` statement?"
+        assert self.pause_threshold >= self.non_speaking_duration >= 0
+
+        seconds_per_buffer = (source.CHUNK + 0.0) / source.SAMPLE_RATE
+        pause_buffer_count = int(math.ceil(self.pause_threshold / seconds_per_buffer)) # number of buffers of non-speaking audio during a phrase, before the phrase should be considered complete
+        phrase_buffer_count = int(math.ceil(self.phrase_threshold / seconds_per_buffer)) # minimum number of buffers of speaking audio before we consider the speaking audio a phrase
+        non_speaking_buffer_count = int(math.ceil(self.non_speaking_duration / seconds_per_buffer)) # maximum number of buffers of non-speaking audio to retain before and after a phrase
+
+        # read audio input for phrases until there is a phrase that is long enough
+        elapsed_time = 0 # number of seconds of audio read
+        buffer = b"" # an empty buffer means that the stream has ended and there is no data left to read
+        while True:
+            print("big loop")
+
+            t = threading.Thread(target=endRecordingEventHandler)
+            t.start()
+
+            frames = collections.deque()
+
+            # store audio input until the phrase starts
+
+            while True:
+                print("wait for audio input...")
+                # handle waiting too long for phrase by raising an exception
+                elapsed_time += seconds_per_buffer
+                if timeout and elapsed_time > timeout:
+                    raise WaitTimeoutError("listening timed out while waiting for phrase to start")
+
+                buffer = source.stream.read(source.CHUNK)
+                if len(buffer) == 0: break # reached end of the stream
+                frames.append(buffer)
+                if len(frames) > non_speaking_buffer_count: # ensure we only keep the needed amount of non-speaking buffers
+                    frames.popleft()
+
+                # detect whether speaking has started on audio input
+                energy = audioop.rms(buffer, source.SAMPLE_WIDTH) # energy of the audio signal
+                if energy > self.energy_threshold: break
+
+                # dynamically adjust the energy threshold using assymmetric weighted average
+                if self.dynamic_energy_threshold:
+                    damping = self.dynamic_energy_adjustment_damping ** seconds_per_buffer # account for different chunk sizes and rates
+                    target_energy = energy * self.dynamic_energy_ratio
+                    self.energy_threshold = self.energy_threshold * damping + target_energy * (1 - damping)
+
+            # read audio input until the phrase ends
+            pause_count, phrase_count = 0, 0
+            phrase_start_time = elapsed_time
+            while True:
+                print("reading audio input...")
+                # handle phrase being too long by cutting off the audio
+                elapsed_time += seconds_per_buffer
+                if phrase_time_limit and elapsed_time - phrase_start_time > phrase_time_limit:
+                    break
+
+                buffer = source.stream.read(source.CHUNK)
+                if len(buffer) == 0: break # reached end of the stream
+                frames.append(buffer)
+                phrase_count += 1
+
+                
+                if isEndRecording:
+                    isEndRecording = False
+                    break
+
+                # check if speaking has stopped for longer than the pause threshold on the audio input
+                energy = audioop.rms(buffer, source.SAMPLE_WIDTH) # unit energy of the audio signal within the buffer
+                if energy > self.energy_threshold:
+                    pause_count = 0
+                else:
+                    pause_count += 1
+                if pause_count > pause_buffer_count: # end of the phrase
+                    break
+
+            t.join()
+
+            # check how long the detected phrase is, and retry listening if the phrase is too short
+            phrase_count -= pause_count # exclude the buffers for the pause before the phrase
+            if phrase_count >= phrase_buffer_count or len(buffer) == 0: break # phrase is long enough or we've reached the end of the stream, so stop listening
+
+        # obtain frame data
+        for i in range(pause_count - non_speaking_buffer_count): frames.pop() # remove extra non-speaking frames at the end
+        frame_data = b"".join(list(frames))
+
+        return AudioData(frame_data, source.SAMPLE_RATE, source.SAMPLE_WIDTH)
+        
+
     def listen(self, source, timeout = None, phrase_time_limit = None):
         """
         Records a single phrase from ``source`` (an ``AudioSource`` instance) into an ``AudioData`` instance, which it returns.
@@ -509,6 +617,7 @@ class Recognizer(AudioSource):
             # store audio input until the phrase starts
 
             while True:
+                print("wait for audio input...")
                 # handle waiting too long for phrase by raising an exception
                 elapsed_time += seconds_per_buffer
                 if timeout and elapsed_time > timeout:
@@ -534,6 +643,7 @@ class Recognizer(AudioSource):
             pause_count, phrase_count = 0, 0
             phrase_start_time = elapsed_time
             while True:
+                print("reading audio input...")
                 # handle phrase being too long by cutting off the audio
                 elapsed_time += seconds_per_buffer
                 if phrase_time_limit and elapsed_time - phrase_start_time > phrase_time_limit:
